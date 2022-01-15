5216ba75b6a6110b4d18639c01241cd7ffb60b40
manorz <manor.zvi@campus.technion.ac.il>
2022-01-15 11:18:21 +0200

    pct change + interday change probability + some nice histograms

---
+++ utils.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as pdr
+from typing import Tuple, List, Union
+from pprint import pprint
+import plotly.express as px
+
+
+def read_yahoo(tickers: List[str], starts: List[int], ends: List[Union[None, int]]):
+
+    for i, (ticker, start, end) in enumerate(zip(tickers, starts, ends)):
+        _df = pdr.data.DataReader(ticker, data_source='yahoo', start=start, end=end)
+        if _df.index[0].to_numpy() != np.datetime64(start):
+            print(f'[Warning] requested start data ({np.datetime64(start)}) for {ticker} != provided start data ({_df.index[0].to_numpy()})')
+        _df['Ticker'] = ticker
+        _df['Date'] = _df.index
+        if i == 0:
+            df = _df
+        else:
+            df = pd.concat((df, _df), ignore_index=True)
+    
+    return df
+
+def calc_pct_change_per_col(df: pd.DataFrame, skip_cols: List[str] = ['Ticker', 'Date']):
+    cols = df.columns.tolist()
+    for skip_col in skip_cols:
+        cols.remove(skip_col)
+    for col in cols:
+        df[col + ' Change'] = df.groupby('Ticker')[col].pct_change()
+    df.dropna(inplace=True)
+    return df
+
+def calc_histogram_per_col(
+    df: pd.DataFrame, bins: int = 100, density: bool = True, show: bool = True,
+    skip_cols: List[str] = ['Adj Close', 'Close', 'High', 'Low', 'Open', 'Volume', 'Ticker', 'Date']
+):
+    cols = df.columns.tolist()
+    for skip_col in skip_cols:
+        cols.remove(skip_col)
+    
+    tickers = df['Ticker'].unique().tolist()
+    ticker_df_dict = {ticker : pd.DataFrame for ticker in tickers}
+    for k in ticker_df_dict.keys():
+        ticker_df_dict[k] = df[:][df['Ticker'] == k]
+    
+    # if show:
+    #     for k, v in ticker_df_dict.items():
+    #         for col in cols:
+    #             fig = px.histogram(v, x=col, title=k)
+    #             fig.show()
+    #     print('[Error] show is deprecated in this method')
+    
+    ticker_hist_dict = {k: {c: {'hist': None, 'bin_edges': None} for c in cols} for k in ticker_df_dict.keys()}
+    
+    for k, v in ticker_df_dict.items():
+        for col in cols:
+            hist, bin_edges = np.histogram(v[col], bins=bins, density=density)
+            ticker_hist_dict[k][col]['hist'] = hist
+            ticker_hist_dict[k][col]['bin_edges'] = bin_edges
+
+    if show:
+        for col in cols:
+            fig, axs = plt.subplots(1, len(ticker_df_dict), sharey=True, tight_layout=True)
+            fig.suptitle(col)
+            for i, (k, v) in enumerate(ticker_df_dict.items()):
+                _, bin_edges = np.histogram(v[col], bins=bins, density=density)
+                axs[i].hist(v[col].to_numpy(), bins=bin_edges, density=True)#bins)#, density=density)#, stacked=True)
+                axs[i].set_title(k)
+            plt.show()
+
+    return ticker_hist_dict
+
+
+
+
+if __name__ == '__main__':
+    starts=['2000-01-01']*2
+    ends=[None]*2
+    tickers = ('GOOG', 'AAPL')
+    df = read_yahoo(tickers, starts, ends)
+    df = calc_pct_change_per_col(df)
+    print(df)
+    ticker_hist_dict = calc_histogram_per_col(df)
+    exit(0)
